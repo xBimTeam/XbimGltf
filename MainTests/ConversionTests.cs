8fa90992a0fb153cf3fcc7f0acce17fef81bf85e
Claudio Benghi <claudio.benghi@gmail.com>
2020-02-05 14:01:51 +0100

    [Lib] Releasing for plugin system update.

--- MainTests/ConversionTests.cs
+++ MainTests/ConversionTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xbim.Common.Geometry;
 using Xbim.GLTF;
@@ -18,6 +13,8 @@ namespace MainTests
     {
         [TestMethod]
         [DeploymentItem(@"Files\OneWallTwoWindows.ifc")]
+        [DeploymentItem("x86", "x86")]
+        [DeploymentItem("x64", "x64")]
         public void CanConvertOneFile()
         {
             var ifc = new FileInfo("OneWallTwoWindows.ifc");
