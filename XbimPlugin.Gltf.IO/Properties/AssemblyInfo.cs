23df64f1f870c0450b9f3541341a0f53278fe2ea
Claudio Benghi <claudio.benghi@gmail.com>
2020-02-11 15:20:17 +0100

    [Feature] First plugin published for v51

--- XbimPlugin.Gltf.IO/Properties/AssemblyInfo.cs
+++ XbimPlugin.Gltf.IO/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XbimPlugin.Gltf.IO")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
